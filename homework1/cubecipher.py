be630215a35f26cf0d7b3077aef7fa0b3f8ca3b0
Annamira O'Toole <annamira@uname.net>
2023-10-24 21:03:40 +0200

    fix some bugs in statistical analysis (still doesn't work)

--- homework1/cubecipher.py
+++ homework1/cubecipher.py
@@ -129,40 +129,82 @@ class CubeCipher:
         return output
     
 
-def split_into_digrams(txt):
+def decrypt_with_6_letter_key(ct, key):
     # split into digrams
-    digrams = []
-    while txt:
-        digrams += [txt[:2]]
-        txt = txt[2:]
-    return digrams
+    plaintext = ""
+    pos = 0
+    while pos < len(ct):
+        
+        if ct[pos] not in string.ascii_uppercase:
+            plaintext += ct[pos] # write the skipped characters to ct to preserve strcuture.
+            pos +=1
+            continue
 
+        new_char = chr((ord(ct[pos]) - ord(key[pos % 6]))% 26 + ord('A'))
+        plaintext += new_char
+        pos += 1
+
+    return plaintext
+
+def print_hello():
+    print("alsdkfjas;lfkjas;dlfkja TEST TEST TEST")
 
 def statistical(ct):
 
+    Q2b_mhash='599aa36ad3fb3611ff0e274e4058bc77ccf0f3677558dd5873da64bef4c8dbc9'
+
     # divide ciphertext into strings with characters from each position mod 6 / each column
-    ct_cols = [ct[i::6] for i in range(6)]
+    ct_only_alphabet = ""
+    for c in ct:
+        if c in string.ascii_uppercase:
+            ct_only_alphabet += c
+    ct_cols = [ct_only_alphabet[i::6] for i in range(6)]
+
+    print("ct_cols is ", ct_cols)
+    print()
 
     # count occurences of each letter, store this info somehow
         # find the most common letter in each column
     col_counts = [Counter(col_str) for col_str in ct_cols]
-    col_counts_sorted = [sorted(c, key=lambda x: x[1]) for c in col_counts]
+    print("col_counts is ", col_counts)
+    print()
+
+    col_counts_sorted = [sorted(c.items(), key=lambda x: x[1]) for c in col_counts]
+    print("col_counts_sorted is ", col_counts)
+    print()
 
     # call the most frequent letter in column 0, F0
 
     english_freqs = ['E', 'T', 'A', 'O', 'I', 'N', 'S', 'H', 'R', 'D',
                       'L', 'C', 'U', 'M', 'W', 'F', 'G', 'Y', 'P', 'B',
                         'V', 'K', 'J', 'X', 'Q', 'Z']
-
+    
+    
     # given list of english letters sorted most to least common
-    # for LT in sorted_list:
+    # for EL in english_freqs:
         # assume that F0's decryption is the letter LT
         # solve for what the key letter is = F0 - LT mod 26
         # chr((ord(ct[pos]) - ord(self.cube.get_piece(1, 1, -1).colors[1]))% 26 + ord('A')) 
 
-    # 
+    
+    # FIRST ASSUME ALL COLUMNS ARE "E"
+    key = ""
 
-    pass
+    for col in range(6):
+        print("analyzing column ", col)
+        freq = col_counts_sorted[col][0][0]
+        print("most frequent ciphertext letter in column ", col, " is ", freq)
+        c = chr((ord(freq) - ord("E"))% 26 + ord('A'))
+        key += c
+        print("if that letter is encrypting ", "E", " then the key letter is ", c)
+        print()
+
+    Q2b_m_guess = decrypt_with_6_letter_key(ct, key)
+    print("if we decrypt with the resulting key we get ", Q2b_m_guess)
+    print()
+    #print("Assertion is ", hashlib.sha256(Q2b_m_guess.encode()).hexdigest() == Q2b_mhash)
+
+    return key, Q2b_m_guess
 
 
 def brute_force(ct):
@@ -179,7 +221,7 @@ def brute_force(ct):
 
         pos = 0
         while pos < len(ct):
-            new_char = chr((ord(ct[pos]) - ord(key[pos % 5]))% 26 + ord('A'))
+            new_char = chr((ord(ct[pos]) - ord(key[pos % 6]))% 26 + ord('A'))
             Q2b_m += new_char
             pos += 1
 
@@ -221,10 +263,14 @@ def test_part2():
 
     Q2b_c='  BY NO UHNEI, VWMAPS MEEETBC NXIWJUZ MH SJI OHG,    T GUWKXO JJ IBZSU YJCNCF JDX UCBYJ;   UNE, OES MALU YI IXCGUYMEJ TKNX B SBLA GHYF,    MDR, B OP YP TZLJD WGW LHQEG.   RZV QNX HWE, IWBW EIU UHNEI, QO B FPOJEHGPE RAYHCF,    QJW ALWU CKHHO CKLM FOSKFFZOBU YTE;   ZUP RHF UKNGXO B RWVD-DPCAKLLVBP BG LU JDX WZPH--    LKTJ, XXWM BD UXA KXLTEJ HY EIQP?   BG XZ OKNMS, TQEW MSF IWZX, LT XA LAZPA DBL RSUU EHNLI,    E DXAU QHE FJ MYIUL GFHU LNAQBA   UR EIU QLX ZG JDBL ZJDPFXYU--EJX LSJBHBGR UXA UHI--    BBHHP XF JK LXWM OKN T NPKLEX?   JPK WKX ZMT, OTBO UXA RHFUX, WGW JPKN CTHT QNX MZP MATD    QPH WGREIYJZ MZVWDXK EIQJ LNPU;   OAM RZV VEGBDIUZ MAP HEKLX, HJJD MAP CEJXL LOT PAX MFQG--    IKLZ XKP WTE OKN FLOQCX MZ EE EM?   BY NO UHNEI, IWBW SJI BTMSFH, E MHZL JK MAP MQS,    TGO BHCNXO FQYA VLTU SBMS NO SBYP;   BDZ MAP NKOVNWBH OMKPOWPA, PSJSD BM RBLA MH XZ ZWP,    ALT BWLMPE JDX KPTJ KY FJ MYBX.   RZV QNX HWE, IWBW EIU UHNEI, EJX PZVBZ ATCEBU LNAQEOX    MSBJ UHNC FOA PTD BI OMXLEO WL XGFH;   UXM JPK XTELOSAW TY FUH HG EIU AGW ZG OKNK YPIA--    PALU CWWX JPK OH THGKHER NMURXK?   T IQRX TYTMAKXO UXNXX BVUOMBZOI, WGW EIQP BL POEQZA,    DBYZ ABD GQPAXC; EEJM ZTWU UHNCTUHY TTSI!   ZH RZV JDBGV J'
 
-    key, plaintext = brute_force(Q2b_c)
+    test_ct = "XYABK DJFGL-!"
 
-    print(key)
-    print(plaintext)
+    print("cipher text is ", test_ct)
+
+    key, plaintext = statistical(test_ct)
+
+    # print(key)
+    # print(plaintext)
 
 
 
